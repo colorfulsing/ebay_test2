844c32f83d91c00121e1f0b53b820d4f6a305852
ER <colorfulsing@gmail.com>
2020-08-25 18:39:35 -0600

    Fix

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,6 +1,6 @@
 html = Nokogiri.HTML(content)
 product = page['vars']
 
-product['status'] = html.xpath('/html/body/div[3]/div[3]/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/ul[2]/li[1]/span')
+product['status'] = html.css('#vi-itm-cond').text.strip
 
 outputs << product
